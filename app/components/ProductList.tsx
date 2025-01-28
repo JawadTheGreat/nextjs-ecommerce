b5793714df7db041a100175233b864386a31ec2c
Abu Jawad <abujawadsitol@gmail.com>
2025-01-28 22:25:06 +0600

    created categorylist and some changes

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -3,14 +3,126 @@ import Link from "next/link";
 
 const ProductList = () => {
   return (
-    <div className="flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link href="/test" className="relative w-full h-80">
-        <Image
-          src="https://images.pexels.com/photos/29893439/pexels-photo-29893439/free-photo-of-delicious-breakfast-with-bread-and-eggs-dish.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-          alt=""
-          fill
-          sizes="25vw"
-        />
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/29893439/pexels-photo-29893439/free-photo-of-delicious-breakfast-with-bread-and-eggs-dish.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500 ease-in"
+          />
+          <Image
+            src="https://images.pexels.com/photos/255311/pexels-photo-255311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/29893439/pexels-photo-29893439/free-photo-of-delicious-breakfast-with-bread-and-eggs-dish.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500 ease-in"
+          />
+          <Image
+            src="https://images.pexels.com/photos/255311/pexels-photo-255311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/29893439/pexels-photo-29893439/free-photo-of-delicious-breakfast-with-bread-and-eggs-dish.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500 ease-in"
+          />
+          <Image
+            src="https://images.pexels.com/photos/255311/pexels-photo-255311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/29893439/pexels-photo-29893439/free-photo-of-delicious-breakfast-with-bread-and-eggs-dish.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500 ease-in"
+          />
+          <Image
+            src="https://images.pexels.com/photos/255311/pexels-photo-255311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          Add to Cart
+        </button>
       </Link>
     </div>
   );
