5c532db8baaf00917d9288e8a199b78bab18058a
Abu Jawad <abujawadsitol@gmail.com>
2025-02-27 22:59:30 +0600

    feat: Implement product details page with customization options
    - Added  component to display product details
    - Implemented  component to show product images with selection functionality
    - Created  component for selecting product color and size
    - Added  component for adding products to the cart
    
    Closes #50

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Add from "../components/Add";
+import CustomizeProducts from "../components/CustomizeProducts";
 import ProductImages from "../components/ProductImages";
 
 const SinglePage = () => {
@@ -8,7 +10,46 @@ const SinglePage = () => {
         <ProductImages />
       </div>
       {/* TEXTS */}
-      <div className="w-full lg:w-1/2 flex flex-col gap-6">TEXTS</div>
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+          tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+        </p>
+        <div className="h-[2px] bg-gray-100" />
+        <div className="flex items-center gap-4">
+          <h3 className="text-xl text-gray-500 line-through">$59</h3>
+          <h2 className="font-medium text-2xl">$49</h2>
+        </div>
+        <div className="h-[2px] bg-gray-100" />
+        <CustomizeProducts />
+        <Add />
+        <div className="h-[2px] bg-gray-100" />
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quam
+            doloremque quis, modi enim eius velit debitis aspernatur nemo libero
+            sit quia. Cupiditate delectus quaerat eligendi quas eum nulla sint!
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quam
+            doloremque quis, modi enim eius velit debitis aspernatur nemo libero
+            sit quia. Cupiditate delectus quaerat eligendi quas eum nulla sint!
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quam
+            doloremque quis, modi enim eius velit debitis aspernatur nemo libero
+            sit quia. Cupiditate delectus quaerat eligendi quas eum nulla sint!
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- app/components/ProductImages.tsx
+++ app/components/ProductImages.tsx
@@ -1,4 +1,58 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+const images = [
+  {
+    id: 1,
+    url: "https://images.pexels.com/photos/19651067/pexels-photo-19651067/free-photo-of-slice-of-cake-on-a-plate.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  },
+  {
+    id: 2,
+    url: "https://images.pexels.com/photos/5593497/pexels-photo-5593497.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  },
+  {
+    id: 3,
+    url: "https://images.pexels.com/photos/30406031/pexels-photo-30406031/free-photo-of-misty-winter-forest-road-in-thuringen.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  },
+  {
+    id: 4,
+    url: "https://images.pexels.com/photos/30862653/pexels-photo-30862653/free-photo-of-charming-village-scene-in-lopud-croatia.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  },
+];
+
 const ProductImages = () => {
-  return <div>ProductImages</div>;
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div className="">
+      <div className="h-[500px] relative">
+        <Image
+          src={images[index].url}
+          alt=""
+          sizes="50vw"
+          fill
+          className="object-cover rounded-md"
+        />
+      </div>
+      <div className="flex justify-between gap-4 mt-8">
+        {images.map((img, i) => (
+          <div
+            key={img.id}
+            onClick={() => setIndex(i)}
+            className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
+          >
+            <Image
+              src={img.url}
+              alt=""
+              sizes="30vw"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default ProductImages;
