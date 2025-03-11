c9c57a04b4278549d9e1ebf9fb4b83019abe1145
Abu Jawad <abujawadsitol@gmail.com>
2025-03-12 01:17:01 +0600

    created wixContext, added context, fetched data from api

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+import { useContext, useEffect } from "react";
 import CategoryList from "./components/CategoryList";
 import ProductList from "./components/ProductList";
 import Slider from "./components/Slider";
+import { WixClientContext } from "./context/wixContext";
 
 const HomePage = () => {
+  const wixClient = useContext(WixClientContext);
+  useEffect(() => {
+    const getProducts = async () => {
+      const res = await wixClient.products.queryProducts().find();
+      console.log(res);
+    };
+    getProducts();
+  }, [wixClient]);
+
   return (
     <div className="">
       <Slider />
