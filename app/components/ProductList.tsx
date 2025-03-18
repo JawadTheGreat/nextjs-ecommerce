ffa86740fb4569a7e4c8363fa8c29bd852513ad2
Abu Jawad <abujawadsitol@gmail.com>
2025-03-18 17:51:48 +0600

    some changes

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -1,129 +1,68 @@
 import Image from "next/image";
 import Link from "next/link";
+import { wixClientServer } from "../lib/wixClientServer";
+import { products } from "@wix/stores";
 
-const ProductList = () => {
+const ProductList = async ({
+  categoryId,
+  limit,
+}: {
+  categoryId: string;
+  limit?: number;
+}) => {
+  const PRODUCTS_PER_PAGE = 20;
+
+  const wixClient = await wixClientServer();
+  const res = await wixClient.products
+    .queryProducts()
+    .eq("collectionIds", categoryId)
+    .limit(limit || PRODUCTS_PER_PAGE)
+    .find();
+  console.log(res);
+
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="https://images.pexels.com/photos/29893439/pexels-photo-29893439/free-photo-of-delicious-breakfast-with-bread-and-eggs-dish.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500 ease-in"
-          />
-          <Image
-            src="https://images.pexels.com/photos/255311/pexels-photo-255311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-          />
-        </div>
+      {res.items.map((product: products.Product) => (
+        <Link
+          href={"/" + product.slug}
+          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+          key={product._id}
+        >
+          <div className="relative w-full h-80">
+            <Image
+              src={product.media?.mainMedia?.image?.url || "/product.png"}
+              alt=""
+              fill
+              sizes="25vw"
+              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500 ease-in"
+            />
+            {product.media?.items && (
+              <Image
+                src={product.media?.items[1].image?.url || "/product.png"}
+                alt=""
+                fill
+                sizes="25vw"
+                className="absolute object-cover rounded-md"
+              />
+            )}
+          </div>
 
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500">My description</div>
-        <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">
-          Add to Cart
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="https://images.pexels.com/photos/29893439/pexels-photo-29893439/free-photo-of-delicious-breakfast-with-bread-and-eggs-dish.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500 ease-in"
-          />
-          <Image
-            src="https://images.pexels.com/photos/255311/pexels-photo-255311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-          />
-        </div>
-
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500">My description</div>
-        <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">
-          Add to Cart
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="https://images.pexels.com/photos/29893439/pexels-photo-29893439/free-photo-of-delicious-breakfast-with-bread-and-eggs-dish.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500 ease-in"
-          />
-          <Image
-            src="https://images.pexels.com/photos/255311/pexels-photo-255311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-          />
-        </div>
-
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500">My description</div>
-        <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">
-          Add to Cart
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="https://images.pexels.com/photos/29893439/pexels-photo-29893439/free-photo-of-delicious-breakfast-with-bread-and-eggs-dish.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500 ease-in"
-          />
-          <Image
-            src="https://images.pexels.com/photos/255311/pexels-photo-255311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-          />
-        </div>
-
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500">My description</div>
-        <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">
-          Add to Cart
-        </button>
-      </Link>
+          <div className="flex justify-between">
+            <span className="font-medium">{product.name}</span>
+            <span className="font-semibold">{product.priceData?.price}</span>
+          </div>
+          {product.additionalInfoSections && (
+            <div className="text-sm text-gray-500">
+              {product.additionalInfoSections?.find(
+                (section: any) => section.title === "shortDesc"
+              )?.description || ""}
+            </div>
+          )}
+          <button className="w-max rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">
+            Add to Cart
+          </button>
+        </Link>
+      ))}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 // "use client";
 
 // import { useEffect } from "react";
+import { Suspense } from "react";
 import CategoryList from "./components/CategoryList";
 import ProductList from "./components/ProductList";
 import Slider from "./components/Slider";
-import { wixClientServer } from "./lib/wixClientServer";
+// import { wixClientServer } from "./lib/wixClientServer";
 // import useWixClient from "./hooks/useWixClient";
 
 const HomePage = async () => {
@@ -17,16 +18,21 @@ const HomePage = async () => {
   //   getProducts();
   // }, [wixClient]);
 
-  const wixClient = await wixClientServer();
-  const res = await wixClient.products.queryProducts().find();
-  console.log(res);
+  // const wixClient = await wixClientServer();
+  // const res = await wixClient.products.queryProducts().find();
+  // console.log(res);
 
   return (
     <div className="">
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
-        <ProductList />
+        <Suspense fallback={"Loading..."}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
       </div>
       <div className="mt-24">
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
@@ -36,7 +42,7 @@ const HomePage = async () => {
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-        <ProductList />
+        <ProductList categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!} />
       </div>
     </div>
   );
