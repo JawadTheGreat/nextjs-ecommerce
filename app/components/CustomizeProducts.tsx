45bf2190a23cb9003c28a2d2baf471445de41e7b
Abu Jawad <abujawadsitol@gmail.com>
2025-06-15 22:38:22 +0600

    some changes

--- app/components/CustomizeProducts.tsx
+++ app/components/CustomizeProducts.tsx
@@ -29,7 +29,10 @@ const CustomizeProducts = ({
       if (!variantChoices) return false;
       return Object.entries(choices).every(
         ([key, value]) =>
-          variantChoices[key] === value && variant.stock?.inStock
+          variantChoices[key] === value &&
+          variant.stock?.inStock &&
+          variant.stock?.quantity &&
+          variant.stock?.quantity > 0
       );
     });
   };
@@ -39,17 +42,60 @@ const CustomizeProducts = ({
       {productOptions.map((option) => (
         <div className="flex flex-col gap-4" key={option.name}>
           <h4 className="font-medium">Choose a {option.name}</h4>
-          {option.choices?.map((choice) => (
-            <div
-              className=""
-              key={choice.value}
-              onClick={() => handleOptionSelect(option.name!, choice.value!)}
-            >
-              {choice.description}
-            </div>
-          ))}
+          <ul className="flex items-center gap-3">
+            {option.choices?.map((choice) => {
+              const disabled = !isVarirantInStock({
+                ...selectedOptions,
+                [option.name!]: choice.description!,
+              });
+              const selected =
+                selectedOptions[option.name!] === choice.description;
+
+              const clickHandler = disabled
+                ? undefined
+                : () => handleOptionSelect(option.name!, choice.description!);
+
+              return option.name === "Color" ? (
+                <li
+                  className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative "
+                  style={{
+                    backgroundColor: choice.description,
+                    cursor: disabled ? "not-allowed" : "pointer",
+                  }}
+                  key={choice.description}
+                  onClick={clickHandler}
+                >
+                  {selected && (
+                    <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                  )}
+                  {disabled && (
+                    <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                  )}
+                </li>
+              ) : (
+                <li
+                  className="ring-1 ring-lama text-lama rounded-md py-1 px-4 text-sm "
+                  style={{
+                    backgroundColor: selected
+                      ? "#f35c7a"
+                      : disabled
+                      ? " #fbcfe8"
+                      : "#fff",
+                    color: selected || disabled ? "white" : "#f35c7a",
+                    cursor: disabled ? "not-allowed" : "pointer",
+                    boxShadow: disabled ? "none" : "",
+                  }}
+                  key={choice.description}
+                  onClick={clickHandler}
+                >
+                  {choice.description}
+                </li>
+              );
+            })}
+          </ul>
         </div>
       ))}
+      {/* COLORS */}
       {/* <h4 className="font-medium">Choose a {option.name}</h4>
       <ul className="flex items-center gap-3">
         <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-red-500">
@@ -60,6 +106,7 @@ const CustomizeProducts = ({
           <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
         </li>
       </ul> */}
+      {/* OTHERS */}
       {/* <h4 className="font-medium">Choose a size</h4>
       <ul className="flex items-center gap-3">
         <li className="ring-1 ring-lama text-lama rounded-md py-1 px-4 text-sm cursor-pointer">
