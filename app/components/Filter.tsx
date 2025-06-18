aa3e5f033da37b65e8699e10d273e7d54f07f5a9
Abu Jawad <abujawadsitol@gmail.com>
2025-06-18 18:14:04 +0600

    feat: Add Filter component for product listing
    - Implemented Filter component with type, price, category, and sort options
    - Integrated filter controls using select and input elements
    - Prepared handlers for updating filters via URL search

--- app/components/Filter.tsx
+++ app/components/Filter.tsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 const Filter = () => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+
   return (
     <div className="mt-12 flex justify-between">
       <div className="flex gap-6 flex-wrap">
@@ -6,6 +14,7 @@ const Filter = () => {
           name="type"
           id=""
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+          onChange={handleFilterChange}
         >
           <option>Type</option>
           <option value="physical">Physical</option>
@@ -16,33 +25,21 @@ const Filter = () => {
           name="min"
           placeholder="min price"
           className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
+          onChange={handleFilterChange}
         />
         <input
           type="text"
           name="max"
           placeholder="max price"
           className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
+          onChange={handleFilterChange}
         />
+
         <select
-          name="size"
+          name="cat"
           id=""
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
-        >
-          <option>Size</option>
-          <option value="">Size</option>
-        </select>
-        <select
-          name="color"
-          id=""
-          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
-        >
-          <option>Color</option>
-          <option value="">Test</option>
-        </select>
-        <select
-          name="ribbon"
-          id=""
-          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+          onChange={handleFilterChange}
         >
           <option>Category</option>
           <option value="">New Arrival</option>
@@ -58,9 +55,10 @@ const Filter = () => {
       </div>
       <div className="">
         <select
-          name=""
+          name="sort"
           id=""
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-white ring-1 ring-gray-400"
+          onChange={handleFilterChange}
         >
           <option>Sort By</option>
           <option value="">Price (low to high)</option>
